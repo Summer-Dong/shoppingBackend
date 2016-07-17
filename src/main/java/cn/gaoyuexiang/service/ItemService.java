0ed1bcc33d9f1fa7bdac8b1d750ee8138942d42f
gaoyx <melo@gaoyuexiang.cn>
2016-07-17 22:39:55 +0800

    [#3] finish ItemService

--- src/main/java/cn/gaoyuexiang/service/ItemService.java
+++ src/main/java/cn/gaoyuexiang/service/ItemService.java
@@ -1,11 +1,22 @@
 package cn.gaoyuexiang.service;
 
 import cn.gaoyuexiang.model.Item;
+import com.fasterxml.jackson.core.type.TypeReference;
 
+import java.io.InputStream;
 import java.util.List;
 
 public class ItemService {
+	private SerializeService serializeService;
+	private final String ITEM_DS = "item.json";
+
+	ItemService(SerializeService serializeService) {
+		this.serializeService = serializeService;
+	}
+
 	public List<Item> getItems() {
-		return null;
+		InputStream inputStream = getClass().getResourceAsStream(ITEM_DS);
+		return serializeService.convertTo(inputStream,
+						new TypeReference<List<Item>>() {});
 	}
 }
