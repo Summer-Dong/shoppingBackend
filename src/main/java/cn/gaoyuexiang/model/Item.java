54946b5a890691977685ef3fe4c09a4987bc15de
gaoyx <melo@gaoyuexiang.cn>
2016-07-17 18:51:57 +0800

    [#3] add Item.java

---
+++ src/main/java/cn/gaoyuexiang/model/Item.java
@@ -0,0 +1,12 @@
+package cn.gaoyuexiang.model;
+
+public class Item {
+
+	private String barcode;
+	private String name;
+	private String unit;
+	private String category;
+	private String subCategory;
+	private Double price;
+
+}
