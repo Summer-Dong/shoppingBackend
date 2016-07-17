65696674f3bfefc1d45ffa5dacbeb420c8c7f334
gaoyx <melo@gaoyuexiang.cn>
2016-07-17 22:34:15 +0800

    [N/A] add SerializeService

---
+++ src/main/java/cn/gaoyuexiang/service/SerializeService.java
@@ -0,0 +1,11 @@
+package cn.gaoyuexiang.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.InputStream;
+
+public class SerializeService {
+	public <T> T convertTo(InputStream inputStream, TypeReference<T> typeRef) {
+		return null;
+	}
+}
