db76de87a68ed84f3de1350eb94ef1941955475c
gaoyx <melo@gaoyuexiang.cn>
2016-07-17 19:15:33 +0800

    [#3] add setters

--- src/main/java/cn/gaoyuexiang/model/Item.java
+++ src/main/java/cn/gaoyuexiang/model/Item.java
@@ -9,4 +9,27 @@ public class Item {
 	private String subCategory;
 	private Double price;
 
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setSubCategory(String subCategory) {
+		this.subCategory = subCategory;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
