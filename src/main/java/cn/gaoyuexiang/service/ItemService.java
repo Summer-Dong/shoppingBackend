cb3cdc6690404f6fc36d4ed93160375bf6d0d1d9
gaoyx <melo@gaoyuexiang.cn>
2016-07-18 22:09:25 +0800

    [#3] complement Autowired annotation

--- src/main/java/cn/gaoyuexiang/service/ItemService.java
+++ src/main/java/cn/gaoyuexiang/service/ItemService.java
@@ -2,6 +2,7 @@ package cn.gaoyuexiang.service;
 
 import cn.gaoyuexiang.model.Item;
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
@@ -12,6 +13,7 @@ public class ItemService {
 	private SerializeService serializeService;
 	private final String ITEM_DS = "item.json";
 
+	@Autowired
 	ItemService(SerializeService serializeService) {
 		this.serializeService = serializeService;
 	}
