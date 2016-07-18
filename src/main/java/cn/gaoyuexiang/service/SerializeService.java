7378b56ebbf7c9f4fe1ea81dffeb80b090acf93b
gaoyx <melo@gaoyuexiang.cn>
2016-07-18 21:48:06 +0800

    [#3] finish SerializaService

--- src/main/java/cn/gaoyuexiang/service/SerializeService.java
+++ src/main/java/cn/gaoyuexiang/service/SerializeService.java
@@ -1,11 +1,23 @@
 package cn.gaoyuexiang.service;
 
+import cn.gaoyuexiang.exception.StoreException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+@Component
 public class SerializeService {
+
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
 	public <T> T convertTo(InputStream inputStream, TypeReference<T> typeRef) {
-		return null;
+		try {
+			return objectMapper.readValue(inputStream, typeRef);
+		} catch (IOException e) {
+			throw new StoreException(e.getMessage());
+		}
 	}
 }
