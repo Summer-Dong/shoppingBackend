242d78ed88dc88b445a1898f74e6753f2c249cff
gaoyx <melo@gaoyuexiang.cn>
2016-07-19 20:29:54 +0800

    [#4] add RuleService

---
+++ src/main/java/cn/gaoyuexiang/service/RuleService.java
@@ -0,0 +1,14 @@
+package cn.gaoyuexiang.service;
+
+import cn.gaoyuexiang.model.Rule;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RuleService {
+
+	public List<Rule> getRules() {
+		return null;
+	}
+}
