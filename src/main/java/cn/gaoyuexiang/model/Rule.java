163e8a73a5e3c7af1281b9fa4ff393bc40fcb77b
gaoyx <melo@gaoyuexiang.cn>
2016-07-19 23:21:09 +0800

    [#4] add Getters and Setters

--- src/main/java/cn/gaoyuexiang/model/Rule.java
+++ src/main/java/cn/gaoyuexiang/model/Rule.java
@@ -6,4 +6,28 @@ public class Rule {
 	private String name;
 	private Integer type;
 	private List<String> barcodes;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public List<String> getBarcodes() {
+		return barcodes;
+	}
+
+	public void setBarcodes(List<String> barcodes) {
+		this.barcodes = barcodes;
+	}
 }
