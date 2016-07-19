bcc2147cdebc56288e258e43f72c38aa60869368
gaoyx <melo@gaoyuexiang.cn>
2016-07-19 23:47:08 +0800

    [#5] add NoSaleItem model

---
+++ src/main/java/cn/gaoyuexiang/model/NoSaleItem.java
@@ -0,0 +1,14 @@
+package cn.gaoyuexiang.model;
+
+public class NoSaleItem {
+	private String name;
+	private String price;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+}
