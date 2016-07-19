9daf9225ccc75e8d4f080473c54b1e0f7b037f61
gaoyx <melo@gaoyuexiang.cn>
2016-07-19 23:03:26 +0800

    [#4] add Constructor

--- src/main/java/cn/gaoyuexiang/service/RuleService.java
+++ src/main/java/cn/gaoyuexiang/service/RuleService.java
@@ -1,12 +1,20 @@
 package cn.gaoyuexiang.service;
 
 import cn.gaoyuexiang.model.Rule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RuleService {
+
+	private SerializeService serializeService;
+
+	@Autowired
+	public RuleService(SerializeService serializeService) {
+		this.serializeService = serializeService;
+	}
 
 	public List<Rule> getRules() {
 		return null;
