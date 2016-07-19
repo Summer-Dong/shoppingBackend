bbd2b020f965605ff860415e4d4ba68b3b7d9d16
gaoyx <melo@gaoyuexiang.cn>
2016-07-19 23:34:31 +0800

    [#5] add Constructor

--- src/main/java/cn/gaoyuexiang/controller/PaymentController.java
+++ src/main/java/cn/gaoyuexiang/controller/PaymentController.java
@@ -1,9 +1,15 @@
 package cn.gaoyuexiang.controller;
 
+import cn.gaoyuexiang.service.PaymentService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
+	private PaymentService paymentService;
+
+	public PaymentController(PaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
 }
