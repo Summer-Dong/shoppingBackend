efe4e842f66a95d346a4fa107de271bbb062dc37
gaoyx <melo@gaoyuexiang.cn>
2016-07-19 23:17:45 +0800

    [#3] finish getRules method

--- src/main/java/cn/gaoyuexiang/service/RuleService.java
+++ src/main/java/cn/gaoyuexiang/service/RuleService.java
@@ -1,14 +1,17 @@
 package cn.gaoyuexiang.service;
 
 import cn.gaoyuexiang.model.Rule;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
 import java.util.List;
 
 @Service
 public class RuleService {
 
+	private static final String RULE_DS = "/rule.json";
 	private SerializeService serializeService;
 
 	@Autowired
@@ -17,6 +20,8 @@ public class RuleService {
 	}
 
 	public List<Rule> getRules() {
-		return null;
+		InputStream stream = getClass().getResourceAsStream(RULE_DS);
+		return serializeService.convertTo(stream, new TypeReference<List<Rule>>() {
+		});
 	}
 }
