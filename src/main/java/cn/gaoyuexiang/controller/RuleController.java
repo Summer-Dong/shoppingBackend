a3a83771658439e33b55ec03894807cf395813d4
gaoyx <melo@gaoyuexiang.cn>
2016-07-19 20:30:31 +0800

    [#4] add RuleController

---
+++ src/main/java/cn/gaoyuexiang/controller/RuleController.java
@@ -0,0 +1,24 @@
+package cn.gaoyuexiang.controller;
+
+import cn.gaoyuexiang.model.Rule;
+import cn.gaoyuexiang.service.RuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rules")
+public class RuleController {
+	private RuleService ruleService;
+
+	@Autowired
+	public RuleController(RuleService ruleService) {
+		this.ruleService = ruleService;
+	}
+
+	public List<Rule> getRules() {
+		return ruleService.getRules();
+	}
+}
