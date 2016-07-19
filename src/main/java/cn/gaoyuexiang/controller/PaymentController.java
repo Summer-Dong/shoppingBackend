dd28a5363e215cb42e7fd5bb142eda227bb737f8
gaoyx <melo@gaoyuexiang.cn>
2016-07-19 23:33:09 +0800

    [#5] add PaymentController

---
+++ src/main/java/cn/gaoyuexiang/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package cn.gaoyuexiang.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+}
