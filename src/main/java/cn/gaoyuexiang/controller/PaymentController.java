391aac70835f37c8e35cd40b92b10d6e679a43e6
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 15:32:58 +0800

    [#5] add spring annotations

--- src/main/java/cn/gaoyuexiang/controller/PaymentController.java
+++ src/main/java/cn/gaoyuexiang/controller/PaymentController.java
@@ -3,7 +3,9 @@ package cn.gaoyuexiang.controller;
 import cn.gaoyuexiang.model.PaymentRequest;
 import cn.gaoyuexiang.model.PaymentResult;
 import cn.gaoyuexiang.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 	private PaymentService paymentService;
 
+	@Autowired
 	public PaymentController(PaymentService paymentService) {
 		this.paymentService = paymentService;
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
 	public PaymentResult payment(PaymentRequest paymentRequest) {
 		return paymentService.payment(paymentRequest);
 	}
