88870b91a4f3703b6881a6a6c537c0053ce08dde
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 15:30:49 +0800

    [#5] add payment method in controller

--- src/main/java/cn/gaoyuexiang/controller/PaymentController.java
+++ src/main/java/cn/gaoyuexiang/controller/PaymentController.java
@@ -1,5 +1,7 @@
 package cn.gaoyuexiang.controller;
 
+import cn.gaoyuexiang.model.PaymentRequest;
+import cn.gaoyuexiang.model.PaymentResult;
 import cn.gaoyuexiang.service.PaymentService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +13,9 @@ public class PaymentController {
 
 	public PaymentController(PaymentService paymentService) {
 		this.paymentService = paymentService;
+	}
+
+	public PaymentResult payment(PaymentRequest paymentRequest) {
+		return paymentService.payment(paymentRequest);
 	}
 }
