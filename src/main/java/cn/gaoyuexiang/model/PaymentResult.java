e758b38490bed2c0da890191af68afc8c54dceea
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 17:02:59 +0800

    [#5] add equals and hashcode method

--- src/main/java/cn/gaoyuexiang/model/PaymentResult.java
+++ src/main/java/cn/gaoyuexiang/model/PaymentResult.java
@@ -23,4 +23,36 @@ public class PaymentResult {
 	public double getTotal() {
 		return total;
 	}
+
+	public void add(PaymentResult other) {
+
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		PaymentResult result = (PaymentResult) o;
+
+		if (Double.compare(result.getSale(), getSale()) != 0) return false;
+		if (Double.compare(result.getTotal(), getTotal()) != 0) return false;
+		if (getResultItems() != null ? !getResultItems().equals(result.getResultItems()) : result.getResultItems() != null)
+			return false;
+		return getNoSaleItems() != null ? getNoSaleItems().equals(result.getNoSaleItems()) : result.getNoSaleItems() == null;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		result = getResultItems() != null ? getResultItems().hashCode() : 0;
+		result = 31 * result + (getNoSaleItems() != null ? getNoSaleItems().hashCode() : 0);
+		temp = Double.doubleToLongBits(getSale());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(getTotal());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 }
