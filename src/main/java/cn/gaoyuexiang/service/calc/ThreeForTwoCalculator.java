33662c0170b9f06a3cc70d311fb48f11bed56cf0
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 18:56:10 +0800

    [#5] 完成买二赠一计算

--- src/main/java/cn/gaoyuexiang/service/calc/ThreeForTwoCalculator.java
+++ src/main/java/cn/gaoyuexiang/service/calc/ThreeForTwoCalculator.java
@@ -1,13 +1,43 @@
 package cn.gaoyuexiang.service.calc;
 
-import cn.gaoyuexiang.model.PaymentItem;
-import cn.gaoyuexiang.model.PaymentResult;
+import cn.gaoyuexiang.model.*;
 
 import java.util.List;
 
 public class ThreeForTwoCalculator implements Calculator {
 	@Override
 	public PaymentResult calculate(List<PaymentItem> paymentItems) {
-		return null;
+		PaymentResult result = new PaymentResult();
+		for (PaymentItem item :
+						paymentItems) {
+			Rule rule = item.getRule();
+			Integer type = rule.getType();
+			if (type == 1) {
+				addToResult(result, item);
+			}
+		}
+		return result;
+	}
+
+	private void addToResult(PaymentResult result, PaymentItem item) {
+		Integer amount = item.getAmount();
+		int giveNumber = getGiveNumber(amount);
+		Double price = item.getItem().getPrice();
+		double sale = price * giveNumber;
+		double total = price * amount - sale;
+		ResultItem resultItem = new ResultItem(item.getItem(), amount);
+		resultItem.setTotal(total);
+		ThreeForTwoItem threeForTwoItem =
+						new ThreeForTwoItem(item.getItem(), giveNumber);
+		result.setTotal(result.getTotal() + total);
+		result.setSale(result.getSale() + sale);
+		result.addResultItem(resultItem);
+		result.addThreeForTowItem(threeForTwoItem);
+	}
+
+	private int getGiveNumber(int amount) {
+		if (amount == 1 || amount == 2) return 0;
+		if (amount == 3) return 1;
+		return 1 + getGiveNumber(amount - 3);
 	}
 }
