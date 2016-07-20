73e3c640687b4c41789cdd86175a478248124727
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 15:30:22 +0800

    [#5] add payment method

--- src/main/java/cn/gaoyuexiang/service/PaymentService.java
+++ src/main/java/cn/gaoyuexiang/service/PaymentService.java
@@ -1,7 +1,12 @@
 package cn.gaoyuexiang.service;
 
+import cn.gaoyuexiang.model.PaymentRequest;
+import cn.gaoyuexiang.model.PaymentResult;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+	public PaymentResult payment(PaymentRequest paymentRequest) {
+		return null;
+	}
 }
