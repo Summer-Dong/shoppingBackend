f45e69e58ca15e4a61394bf8b344a6429e9ea544
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 15:31:30 +0800

    [#5] add test method

--- src/test/java/cn/gaoyuexiang/controller/PaymentControllerTest.java
+++ src/test/java/cn/gaoyuexiang/controller/PaymentControllerTest.java
@@ -1,10 +1,17 @@
 package cn.gaoyuexiang.controller;
 
+import cn.gaoyuexiang.model.PaymentRequest;
+import cn.gaoyuexiang.model.PaymentResult;
 import cn.gaoyuexiang.service.PaymentService;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PaymentControllerTest {
@@ -17,6 +24,16 @@ public class PaymentControllerTest {
 	@Before
 	public void setUp() throws Exception {
 		paymentController = new PaymentController(paymentService);
+	}
 
+	@Test
+	public void should_get_return() throws Exception {
+		//request
+		PaymentRequest paymentRequest = new PaymentRequest();
+		//response
+		PaymentResult paymentResult = new PaymentResult();
+		when(paymentService.payment(paymentRequest)).thenReturn(paymentResult);
+		PaymentResult result = paymentController.payment(paymentRequest);
+		assertThat(result, is(paymentResult));
 	}
 }
