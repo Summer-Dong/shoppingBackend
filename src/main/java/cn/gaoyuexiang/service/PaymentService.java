bcf7e21f310178efe9efc2b824ce86a544577704
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 17:04:57 +0800

    [#5] finish payment method in service

--- src/main/java/cn/gaoyuexiang/service/PaymentService.java
+++ src/main/java/cn/gaoyuexiang/service/PaymentService.java
@@ -1,12 +1,35 @@
 package cn.gaoyuexiang.service;
 
+import cn.gaoyuexiang.model.PaymentItem;
 import cn.gaoyuexiang.model.PaymentRequest;
 import cn.gaoyuexiang.model.PaymentResult;
+import cn.gaoyuexiang.service.calc.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class PaymentService {
+	private PaymentMapper paymentMapper;
+	private List<Calculator> calculators = new ArrayList<>();
+
+	@Autowired
+	public PaymentService(PaymentMapper paymentMapper, Calculator... calculators) {
+		this.paymentMapper = paymentMapper;
+		Collections.addAll(this.calculators, calculators);
+	}
+
 	public PaymentResult payment(PaymentRequest paymentRequest) {
-		return null;
+		List<PaymentItem> paymentItems = paymentMapper.map(paymentRequest);
+		PaymentResult paymentResult = new PaymentResult();
+		for (Calculator calculator :
+						calculators) {
+			PaymentResult result = calculator.calculate(paymentItems);
+			paymentResult.add(result);
+		}
+		return paymentResult;
 	}
 }
