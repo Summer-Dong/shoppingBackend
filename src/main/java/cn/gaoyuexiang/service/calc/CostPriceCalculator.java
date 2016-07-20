9d1f4141ee227dd4154c6c885743bd620ca1a2fe
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 19:46:05 +0800

    [#5]实现无优惠商品计算

--- src/main/java/cn/gaoyuexiang/service/calc/CostPriceCalculator.java
+++ src/main/java/cn/gaoyuexiang/service/calc/CostPriceCalculator.java
@@ -2,12 +2,21 @@ package cn.gaoyuexiang.service.calc;
 
 import cn.gaoyuexiang.model.PaymentItem;
 import cn.gaoyuexiang.model.PaymentResult;
+import cn.gaoyuexiang.model.ResultItem;
 
 import java.util.List;
 
 public class CostPriceCalculator implements Calculator {
 	@Override
 	public PaymentResult calculate(List<PaymentItem> paymentItems) {
-		return null;
+		PaymentResult result = new PaymentResult();
+		paymentItems.stream().filter(item -> item.getRule() == null).forEach(item -> {
+			double total = item.getItem().getPrice() * item.getAmount();
+			ResultItem resultItem = new ResultItem(item.getItem(), item.getAmount());
+			resultItem.setTotal(total);
+			result.addResultItem(resultItem);
+			result.setTotal(result.getTotal() + total);
+		});
+		return result;
 	}
 }
