e30ebed3104d0629e7c8ac87d978ffe973c962e6
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 17:05:19 +0800

    [#5] add PaymentMapper as Service

---
+++ src/main/java/cn/gaoyuexiang/service/PaymentMapper.java
@@ -0,0 +1,14 @@
+package cn.gaoyuexiang.service;
+
+import cn.gaoyuexiang.model.PaymentItem;
+import cn.gaoyuexiang.model.PaymentRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentMapper {
+	public List<PaymentItem> map(PaymentRequest paymentRequest) {
+		return null;
+	}
+}
