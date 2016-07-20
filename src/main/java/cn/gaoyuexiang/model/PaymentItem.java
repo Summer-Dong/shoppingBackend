37f11e1d9f474ca68ae7a2607bc88a7f09b5834a
gaoyx <melo@gaoyuexiang.cn>
2016-07-20 17:36:05 +0800

    [#5] add Getters and Setters

--- src/main/java/cn/gaoyuexiang/model/PaymentItem.java
+++ src/main/java/cn/gaoyuexiang/model/PaymentItem.java
@@ -4,4 +4,26 @@ public class PaymentItem {
 	private Item item;
 	private Integer amount;
 	private Rule rule;
+
+	public PaymentItem(Item item, int amount) {
+		this.item = item;
+		this.amount = amount;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setRule(Rule rule) {
+		this.rule = rule;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
 }
