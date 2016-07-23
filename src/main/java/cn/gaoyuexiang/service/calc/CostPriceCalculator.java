32783151861da8e58d6a470bafb5c6eea83c93cb
gaoyx <melo@gaoyuexiang.cn>
2016-07-23 08:46:17 +0800

    [#5] refactor

--- src/main/java/cn/gaoyuexiang/service/calc/CostPriceCalculator.java
+++ src/main/java/cn/gaoyuexiang/service/calc/CostPriceCalculator.java
@@ -12,13 +12,17 @@ public class CostPriceCalculator implements Calculator {
 	@Override
 	public PaymentResult calculate(List<PaymentItem> paymentItems) {
 		PaymentResult result = new PaymentResult();
-		paymentItems.stream().filter(item -> item.getRule() == null).forEach(item -> {
-			double total = item.getItem().getPrice() * item.getAmount();
-			ResultItem resultItem = new ResultItem(item.getItem(), item.getAmount());
-			resultItem.setTotal(total);
-			result.addResultItem(resultItem);
-			result.setTotal(result.getTotal() + total);
-		});
+		paymentItems
+						.stream()
+						.filter(item -> item.getRule() == null)
+						.forEach(item -> {
+							double total = item.getItem().getPrice() * item.getAmount();
+							ResultItem resultItem = new ResultItem(item.getItem()
+											, item.getAmount());
+							resultItem.setTotal(total);
+							result.addResultItem(resultItem);
+							result.setTotal(result.getTotal() + total);
+						});
 		return result;
 	}
 }
--- src/main/java/cn/gaoyuexiang/service/calc/DiscountCalculator.java
+++ src/main/java/cn/gaoyuexiang/service/calc/DiscountCalculator.java
@@ -16,18 +16,22 @@ public class DiscountCalculator implements Calculator {
 						paymentItems) {
 			if (item.getRule() == null) continue;
 			if (item.getRule().getType() == 2) {
-				Integer amount = item.getAmount();
-				Double price = item.getItem().getPrice();
-				double total = price * amount * 0.95;
-				double sale = price * amount * 0.05;
-				ResultItem resultItem = new ResultItem(item.getItem(), amount);
-				resultItem.setSave(sale);
-				resultItem.setTotal(total);
-				result.addResultItem(resultItem);
-				result.setTotal(result.getTotal() + total);
-				result.setSale(result.getSale() + sale);
+				addToResult(result, item);
 			}
 		}
 		return result;
+	}
+
+	private void addToResult(PaymentResult result, PaymentItem item) {
+		Integer amount = item.getAmount();
+		Double price = item.getItem().getPrice();
+		double total = price * amount * 0.95;
+		double sale = price * amount * 0.05;
+		ResultItem resultItem = new ResultItem(item.getItem(), amount);
+		resultItem.setSave(sale);
+		resultItem.setTotal(total);
+		result.addResultItem(resultItem);
+		result.setTotal(result.getTotal() + total);
+		result.setSale(result.getSale() + sale);
 	}
 }
